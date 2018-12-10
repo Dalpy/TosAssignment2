f7b20598aa30bb386385c2650be3ccad44e609f2
Dalpy <dalpy97@gmail.com>
2018-12-10 23:30:05 +0100

    tabs to spaces

--- src/main/java/it/unipd/tos/model/MenuItem.java
+++ src/main/java/it/unipd/tos/model/MenuItem.java
@@ -5,29 +5,29 @@ package it.unipd.tos.model;
 
 public class MenuItem {
 
-	public enum itemType {
-		Pizze, Primi
-	};
+    public enum itemType {
+        Pizze, Primi
+    };
 
-	private itemType type;
-	private String name;
-	private double price;
+    private itemType type;
+    private String name;
+    private double price;
 
-	public MenuItem(itemType type, String name, double price) {
-		this.type = type;
-		this.name = name;
-		this.price = price;
-	}
+    public MenuItem(itemType type, String name, double price) {
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
 
-	public itemType getItemType() {
-		return type;
-	}
+    public itemType getItemType() {
+        return type;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public double getPrice() {
+        return price;
+    }
 }
